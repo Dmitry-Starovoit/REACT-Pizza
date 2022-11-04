bc055482812c76e1395a76b88544f23110659561
Dmitry-Starovoit <113298458+Dmitry-Starovoit@users.noreply.github.com>
2022-11-04 19:40:02 +0200

    Added JSX components and SCSS files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import Categories from './components/Categories/Categories';
+import Header from './components/Header/Header';
+import Pizza_Block from './components/Pizza_Block/Pizza_Block';
+import Sort from './components/Sort/Sort';
+import './scss/app.scss';
 
-function App() {
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="wrapper">
+      <Header />
+      <div class="content">
+        <div class="container">
+          <div class="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 class="content__title">Все пиццы</h2>
+          <div class="content__items">
+            <Pizza_Block
+              title="Карбонара"
+              price="194 грн"
+              image="https://starwood.vn.ua/wp-content/uploads/2020/05/Karbonara.jpg"
+            />
+            <Pizza_Block
+              title="Гавайская"
+              price="187 грн"
+              image="https://starwood.vn.ua/wp-content/uploads/2018/07/Gavajska.jpg"
+            />
+            <Pizza_Block
+              title="Салями"
+              price="167 грн"
+              image="https://starwood.vn.ua/wp-content/uploads/2018/06/Salyami.jpg"
+            />
+            <Pizza_Block
+              title="Маргарита"
+              price="134 грн"
+              image="https://starwood.vn.ua/wp-content/uploads/2018/06/Margarita-1.jpg"
+            />
+            <Pizza_Block
+              title="4 сыра"
+              price="237 грн"
+              image="https://starwood.vn.ua/wp-content/uploads/2018/05/4sira.jpg"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
